bd5391f0829537c9d700d1eb2c8eb80326326ff6
kallip <kallinpettit@gmail.com>
2018-07-30 15:07:15 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,15 @@
 # person.rb
+class Person
+  def initialize (persons_name)
+    @name = persons_name
+  end
+
+  def name=(persons_name)
+    @name = persons_name
+  end
+
+  def name
+    @name
+  end
+  
+end
